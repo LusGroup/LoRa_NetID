1da71fc61d3439223b9089848184aece4cdc390e
JM Jiang <815068542@qq.com>
2023-08-17 16:16:52 +0800

    add a file to simulate conventional lora packet

---
+++ LoRa_simulation.m
@@ -0,0 +1,36 @@
+close all;
+clear all;
+freq = 868e6;
+bw = 250e3;
+fs = 500e3;
+sf = 7;       % sampling rate 1 MHz
+
+phy = LoRaPHY(freq,sf,bw,fs);
+phy.has_header = 1;
+phy.CR = 3;
+phy.CRC = 1;
+phy.preamble_len = 6;  
+% preamble: 8 basic upchirps
+% data = phy.Ref_upchirp;
+
+% LoRa_Analyse.plot_timefrequency(phy.Ref_upchirp,phy.Fs,phy.SF,phy.BW);
+% data is 'hello world,this is a message by simulating in maltab'
+data = [104 101 108 108 111 32 119 111 114 108 100 44 116 104 105 115 32 105 115 32 97 32 109 101 115 115 97 103 101 32 98 121 32 115 105 109 117 108 97 116 105 110 103 32 105 110 32 109 97 108 116 97 98];
+symbols = phy.encode(data.');
+sig = phy.modulate(symbols);
+% 在signal前后加一些底噪
+% pre = zeros(round(0.2*phy.sample_num_per_symbol),1);
+% pre = awgn(pre,10,0);
+% sig = [pre;sig;pre];
+
+% save sig to file 
+filename =  "../sig/lora_packet.cfile";
+LoRaPHY.write(sig,filename);
+
+% read sig from file
+sig = LoRaPHY.read_file(filename);
+LoRaPHY.plot_timefrequency(sig,fs,sf,bw);
+[symbols_d, cfo, netid] = phy.demodulate(sig);
+[data, checksum] = phy.decode(symbols_d);
+fprintf("[decode] data:\n");
+LoRaPHY.print_payload(data);
--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 
 # 使用说明
 + LoRaPHY.m 文件包含了LoRa的调制、解调过程。并且实现了一些用于 debug 的画图、读取iq信号等基本功能。
-+ general_lora_packet.m 文件实现了生成一个**特殊前导码**的 LoRa 数据包
-+ test_sync_method.m 文件用于测试能否解码具有**特殊前导码**的 LoRa 数据包
++ LoRa_simulation.m 文件实现了生成一个常规的 LoRa 数据包并对其进行解码。
++ general_lora_packet.m 文件实现了生成一个**特殊前导码**的 LoRa 数据包。
++ test_sync_method.m 文件用于测试能否解码具有**特殊前导码**的 LoRa 数据包。
